3bcb42b0cc8cae7ec2c3c242e7d3e3c47edfdf34
Mahmoud Moussa <m.moussa@ratio-software.de>
2022-06-23 11:21:54 +0200

    Projektdateien hinzufügen.

---
+++ Fisi_So_16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] file = File.ReadAllLines("..\\..\\..\\skilift.txt");
+
+        int result = 0;
+        int cnt = 0;
+
+        foreach (string line in file)
+        {
+            if (line.Contains("30201"))
+            {
+                int lastOccurrenceOfPipe = line.LastIndexOf('|');
+
+                int subStr = Int32.Parse(line[(lastOccurrenceOfPipe + 1)..]);
+
+                result += subStr;
+
+                cnt++;
+
+            }
+
+        }
+
+        Console.WriteLine(result + (cnt * 30201));
+    }
+}
+
